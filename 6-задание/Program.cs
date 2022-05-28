0105326cb1df7af629246a5b3d3e9525922e0217
Георгий Князев <saint_georgy@yahoo.com>
2022-05-28 11:28:39 +0300

    Добавьте файлы проекта.

---
+++ 6-задание/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _6_задание
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Сколько золота у вас?");
+            int theAMountOfGoldThePlayerHas = Convert.ToInt32(Console.ReadLine());
+            int theCostOfACrystalInGold = 17;
+            Console.Write("Стоимость кристалов ровна = " + theCostOfACrystalInGold + ". Какое количество кристалов вы хотите купить?  ");
+            int crystalsThatWantToBuy = Convert.ToInt32(Console.ReadLine());
+            int theRemainingGoldInTheWallet = theAMountOfGoldThePlayerHas - (theCostOfACrystalInGold * crystalsThatWantToBuy);
+            Console.WriteLine("У вас теперь " + crystalsThatWantToBuy + " кристалов. Остаток золота в кошельке: " + theRemainingGoldInTheWallet + ".");
+        }
+    }
+}
